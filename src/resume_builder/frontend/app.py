299b583150813bc8dfad7c22de30db990f9110e1
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2025-03-13 02:00:25 -0400

    form controller is a success

--- src/resume_builder/frontend/app.py
+++ src/resume_builder/frontend/app.py
@@ -11,34 +11,42 @@ import json
 from resume_builder.frontend.strings import date_to_string, internal_to_external
 
 
-class Deletable:
-    def __init__(self, to_delete):
-        self.to_delete = to_delete
+class ListInputController:
+    def __init__(self, key, data_model):
+        self.data_model = data_model
+        self.key = key
+        if self.key not in st.session_state:
+            st.session_state[self.key] = []
     
     def __enter__(self):
-        self.delete_ledger = []
+        return self
     
-    def delete(self, i):
-        del self.to_delete[i]
-        st.rerun()
-    
-    def __exit__(self, _, _1, _2):
+    def __exit__(self, exc_type, exc_value, traceback):
         pass
+    
+    def add_item(self):
+        st.session_state[self.key].append(copy.deepcopy(self.data_model))
+    
+    def delete_item(self, index):
+        del st.session_state[self.key][index]
+        st.rerun()
 
 
-def validate_partial_form(partial_form, errors: list, key=None):
+def validate_partial_form(partial_form, errors: list, key=None, idx=None, name=None):
     if isinstance(partial_form, dict):
+        new_name = name if name is not None else partial_form.get("name")
         for (section, item) in partial_form.items():
-            validate_partial_form(item, errors, key=section)
+            validate_partial_form(item, errors, key=section, idx=idx, name=new_name)
     elif isinstance(partial_form, list):
-        for item in partial_form:
-            validate_partial_form(item, errors)
+        for i, item in enumerate(partial_form):
+            validate_partial_form(item, errors, idx=i, name=name)
     elif isinstance(partial_form, str):
         external_key = internal_to_external(key)
         if external_key is None:
             return
         errors.append(safe_assert(
-            checkers.is_not_empty(partial_form) or external_key.endswith("(Optional)"), f"{external_key} required."
+            checkers.is_not_empty(partial_form) or external_key.endswith("(Optional)"), 
+            (f"{external_key} required." if idx is None or name is None else f"{external_key} required for entry {idx + 1} in {name}.")
         ))
 
 
@@ -103,14 +111,9 @@ def main():
             "email": "",
             "linkedin": "",
             "profile": "",
-            "domains": []
+            "domains": [],
+            "is_swe": False
         }
-    
-    if "education_marked_for_deletion" not in st.session_state:
-        st.session_state.education_marked_for_deletion = []
-
-    if "education" not in st.session_state:
-        st.session_state.education = []
 
     if "sections" not in st.session_state:
         st.session_state.sections = dict()
@@ -156,17 +159,18 @@ def main():
 
         st.header("Education Information", divider="grey")
 
-        if st.button("Add Education"):
-            st.session_state.education.append({
-                "institution": "",
-                "institution_location": "",
-                "degree_name": "",
-                "start": datetime.now(),
-                "end": datetime.now(),
-                "relevant_coursework": "",
-            })
+        with ListInputController("education", {
+            "institution": "",
+            "institution_location": "",
+            "degree_name": "",
+            "start": datetime.now(),
+            "end": datetime.now(),
+            "relevant_coursework": "",
+        }) as education_controller:
+            
+            if st.button("Add Education"):
+                education_controller.add_item()
 
-        with Deletable(st.session_state.education) as education_controller:
             for i in range(len(st.session_state.education)):
                 with st.expander(f"**Education Item {i + 1}**", expanded=True):
                     st.session_state.education[i]["institution"] = st.text_input(
@@ -204,30 +208,30 @@ def main():
                     st.session_state.education[i]["completed"] = st.session_state.education[i]["end"] <= datetime.now().date()
 
                     if st.button("Delete", key=f"education_delete_{i}"):
-                        st.session_state.education_marked_for_deletion.append(i)
-                        # education_controller.delete(i)
+                        education_controller.delete_item(i)
 
-        for index in sorted(st.session_state.education_marked_for_deletion, reverse=True):
-            del st.session_state.education[index]
-            st.session_state.education_marked_for_deletion = st.session_state.education_marked_for_deletion[:-1]
-            st.rerun()
              
         st.header("Additional Information")
 
-        is_swe = st.checkbox("I am applying for software engineering (or adjacent) roles")
+        is_swe = st.checkbox(
+            "I am applying for software engineering (or adjacent) roles", 
+            key="is_swe", 
+            value=st.session_state.user_info["is_swe"]
+        )
 
         st.session_state.user_info.update({
             "education": st.session_state.education,
             "core_skill_label": "Languages" if is_swe else "Skills",
             "extra_skill_label": "Technologies" if is_swe else None,
-            "domain_label": "Domains" if is_swe else "Areas of Focus"
+            "domain_label": "Domains" if is_swe else "Areas of Focus",
+            "is_swe": is_swe
         })
 
         st.divider()
         col1, col2 = st.columns([1, 9])
 
         with col1:
-            if st.button("Save"):
+            if st.button("Save", use_container_width=True):
                 pass
         with col2:
             if st.button("Save & Continue", type="primary"):
@@ -235,7 +239,8 @@ def main():
                     "education": st.session_state.education,
                     "core_skill_label": "Languages" if is_swe else "Skills",
                     "extra_skill_label": "Technologies" if is_swe else None,
-                    "domain_label": "Domains" if is_swe else "Areas of Focus"
+                    "domain_label": "Domains" if is_swe else "Areas of Focus",
+                    "is_swe": is_swe
                 })
                 st.session_state.page = 2
                 st.rerun()
@@ -268,93 +273,106 @@ def main():
                 key=f"include_{name}",
                 value=st.session_state.sections[name]["include"]
             )
-            
-            if st.button("Add Item", key=f"add_item_{name}"):
-                section["items"].append({
-                    "organization": "",
-                    "location": "",
-                    "position": "",
-                    "start": datetime.now(),
-                    "end": datetime.now(),
-                    "core_skills": [],
-                    "extra_skills": [],
-                    "description": [],
-                    "still_working": False
-                })
-            
-            for i in range(len(section["items"])):
-                with st.expander(f"**{name.capitalize()} Item {i + 1}**", expanded=True):
-                    st.session_state.sections[name]["items"][i]["organization"] = st.text_input(
-                        internal_to_external("organization"), 
-                        key=f"org_{name}_{i}",
-                        value=st.session_state.sections[name]["items"][i]["organization"]
-                    )
-                    st.session_state.sections[name]["items"][i]["location"] = st.text_input(
-                        internal_to_external("location"), 
-                        key=f"loc_{name}_{i}",
-                        value=st.session_state.sections[name]["items"][i]["location"]
-                    )
-                    st.session_state.sections[name]["items"][i]["position"] = st.text_input(
-                        internal_to_external("position"), 
-                        key=f"pos_{name}_{i}",
-                        value=st.session_state.sections[name]["items"][i]["position"]
-                    )
-                    st.session_state.sections[name]["items"][i]["start"] = st.date_input(
-                        internal_to_external("start"), 
-                        key=f"start_{name}_{i}",
-                        value=st.session_state.sections[name]["items"][i]["start"]
-                    )
-                    st.session_state.sections[name]["items"][i]["end"] = st.date_input(
-                        internal_to_external("end"), 
-                        key=f"end_{name}_{i}",
-                        value=st.session_state.sections[name]["items"][i]["end"],
-                        disabled=st.checkbox(
-                            "I am currently working here", 
-                            key=f"working_{name}_{i}",
-                            value=st.session_state.sections[name]["items"][i]["still_working"]
+            with ListInputController(name, {
+                "organization": "",
+                "location": "",
+                "position": "",
+                "start": datetime.now(),
+                "end": datetime.now(),
+                "core_skills": [],
+                "extra_skills": [],
+                "description": [],
+                "still_working": False
+            }) as section_controller:
+                
+                if st.button("Add Item", key=f"add_item_{name}"):
+                    section_controller.add_item()
+                
+                for i in range(len(st.session_state[name])):
+                    with st.expander(f"**{name.capitalize()} Item {i + 1}**", expanded=True):
+                        st.session_state[name][i]["organization"] = st.text_input(
+                            internal_to_external("organization"), 
+                            key=f"org_{name}_{i}",
+                            value=st.session_state[name][i]["organization"]
                         )
-                    )
-                    st.session_state.sections[name]["items"][i]["core_skills"] = st_tags(
-                        label=internal_to_external("core_skills", fmt=core_skills_label), 
-                        key=f"core_{name}_{i}",
-                        value=st.session_state.sections[name]["items"][i]["core_skills"]
-                    )
-
-                    if extra_skills_label is not None:
-                        st.session_state.sections[name]["items"][i]["extra_skills"] = st_tags(
-                            label=internal_to_external("extra_skills", fmt=extra_skills_label), 
-                            key=f"extra_{name}_{i}",
-                            value=st.session_state.sections[name]["items"][i]["extra_skills"]
+                        st.session_state[name][i]["location"] = st.text_input(
+                            internal_to_external("location"), 
+                            key=f"loc_{name}_{i}",
+                            value=st.session_state[name][i]["location"]
                         )
-
-                    st.text(f"{name.capitalize()} Item {i + 1} Description")
-                    if st.button("Add Point", key=f"add_item_{name}_{i}"):
-                        st.session_state.sections[name]["items"][i]["description"].append({
-                            "summary": "",
-                            "group": 0
-                        })
-
-                    for j in range(len(st.session_state.sections[name]["items"][i]["description"])):
-                        col1, col2 = st.columns([0.01, 0.99])
-                        with col1:
-                            st.markdown(f"•", unsafe_allow_html=True)
-                        with col2:
-                            st.session_state.sections[name]["items"][i]["description"][j]["summary"] = st.text_input(
-                                f"Point {j + 1}", 
-                                key=f"sum_{name}_{i}_{j}",
-                                value=st.session_state.sections[name]["items"][i]["description"][j]["summary"]
+                        st.session_state[name][i]["position"] = st.text_input(
+                            internal_to_external("position"), 
+                            key=f"pos_{name}_{i}",
+                            value=st.session_state[name][i]["position"]
+                        )
+                        st.session_state[name][i]["start"] = st.date_input(
+                            internal_to_external("start"), 
+                            key=f"start_{name}_{i}",
+                            value=st.session_state[name][i]["start"]
+                        )
+                        st.session_state[name][i]["end"] = st.date_input(
+                            internal_to_external("end"), 
+                            key=f"end_{name}_{i}",
+                            value=st.session_state[name][i]["end"],
+                            disabled=st.checkbox(
+                                "I am currently working here", 
+                                key=f"working_{name}_{i}",
+                                value=st.session_state[name][i]["still_working"]
                             )
-        
+                        )
+                        st.session_state[name][i]["core_skills"] = st_tags(
+                            label=internal_to_external("core_skills", fmt=core_skills_label), 
+                            key=f"core_{name}_{i}",
+                            value=st.session_state[name][i]["core_skills"]
+                        )
+
+                        if extra_skills_label is not None:
+                            st.session_state[name][i]["extra_skills"] = st_tags(
+                                label=internal_to_external("extra_skills", fmt=extra_skills_label), 
+                                key=f"extra_{name}_{i}",
+                                value=st.session_state[name][i]["extra_skills"]
+                            )
+
+                        st.text(f"{name.capitalize()} Item {i + 1} Description")
+
+                        with ListInputController(f"{name}_{i}_description", {
+                            "summary": "",
+                            "required_skills": [],
+                            "group": 0
+                        }) as description_controller:
+                            st.session_state[f"{name}_{i}_description"] = st.session_state[name][i]["description"]
+                            if st.button("Add Point", key=f"add_item_{name}_{i}"):
+                                description_controller.add_item()
+
+                            for j in range(len(st.session_state[f"{name}_{i}_description"])):
+                                col1, col2 = st.columns([0.9, 0.1], vertical_alignment="bottom")
+                                with col1:
+                                    st.session_state[f"{name}_{i}_description"][j]["summary"] = st.text_input(
+                                        f"Point {j + 1}", 
+                                        key=f"sum_{name}_{i}_{j}",
+                                        value=st.session_state[f"{name}_{i}_description"][j]["summary"]
+                                    )
+                                with col2:
+                                    if st.button("", icon=":material/delete:", key=f"{name}_{i}_{j}_description_delete"):
+                                        description_controller.delete_item(j)
+                        
+                        st.session_state[name][i]["description"] = copy.deepcopy(st.session_state[f"{name}_{i}_description"])
+                        del st.session_state[f"{name}_{i}_description"]
+
+                        st.divider()
+                        if st.button("Delete", key=f"{name}_{i}_delete"):
+                            section_controller.delete_item(i)
+                            
         st.divider()
         col1, col2, col3 = st.columns([1, 1, 8])
 
         with col1:
-            if st.button("Back"):
+            if st.button("Back", use_container_width=True):
                 st.session_state.page = 1
                 st.rerun()
         
         with col2:
-            if st.button("Save"):
+            if st.button("Save", use_container_width=True):
                 pass
         
         with col3:
@@ -363,6 +381,8 @@ def main():
                     "info": copy.deepcopy(st.session_state.user_info),
                     "sections": copy.deepcopy(list(st.session_state.get("sections", []).values()))
                 }
+                for section in resume_data["sections"]:
+                    section["items"] = st.session_state[section["name"]]
                 resume_data = validate_and_post_process(resume_data)
                 st.json(resume_data)
 
