3b2ac9c4746e882729c163dc86334e6afb4bc7b6
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2025-03-19 01:00:47 -0400

    works

--- src/resume_builder/main.py
+++ src/resume_builder/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
 import io
@@ -9,10 +11,18 @@ from resume_builder.pipeline import run_pipeline
 APP = FastAPI()
 
 
+APP.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
+
 class GenerationRequest(BaseModel):
     user_id: str
     link: str
-    theme: str
     desc: str
 
 
@@ -21,12 +31,11 @@ def ping():
     return {"Hello": "World"}
 
 
-@APP.get("/generate")
+@APP.post("/generate")
 def generate(params: GenerationRequest):
     
-    file = run_pipeline(params.user_id, params.link, params.theme, params.desc)
+    file = run_pipeline(params.user_id, params.link, params.desc)
 
     if file:
         with open(file, "rb") as fh:
-            f = io.BytesIO(fh.read())
-            return Response(f, media_type="application/pdf")
+            return FileResponse(file, media_type="application/pdf", filename=file)
--- src/resume_builder/pipeline.py
+++ src/resume_builder/pipeline.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 from validator_collection import validators
@@ -18,62 +19,35 @@ from resume_builder.templates.template import LatexTemplate
 load_dotenv()
 
 
-if os.environ.get("ENVIRONMENT"):
-    ENVIRONMENT = "local"
-    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-    AWS_SECRET_KEY_ID = os.environ["AWS_SECRET_KEY_ID"]
-    REDIRECT_URI = os.environ["REDIRECT_URI"]
-    GOOGLE_CLOUD_CREDENTIALS = json.loads(os.environ["GOOGLE_CLOUD_CREDENTIALS"])
-    with open("credentials.json", "w+") as f:
-        f.write(json.dumps(GOOGLE_CLOUD_CREDENTIALS))
-else:
-    ENVIRONMENT = "production"
-
-AWS_S3_BUCKET = "user-resume-info-resume-builder-tailor"
-
-if ENVIRONMENT == "local":
-    S3_CLIENT = boto3.client(
-        's3', 
-        aws_access_key_id=AWS_ACCESS_KEY_ID, 
-        aws_secret_access_key=AWS_SECRET_KEY_ID
-    )
-else:
-    S3_CLIENT = boto3.client('s3')
-
-
-DOMAIN_TO_TEMPLATE = {
-    "workday": "workday_default"
-}
+def domain_to_template(domain, default):
+    if "workday" in domain:
+        return "workday_default"
+    return default
 
 
 def to_camel_case(snake_str):
     return "".join(x.capitalize() for x in snake_str.lower().split("_"))
 
 
-def run_pipeline(user_id, link, theme, posting):
-    domain = validators.domain(link)
+def run_pipeline(user_id, link, posting):
+    print(user_id)
+    print(link)
+    print(posting)
 
-    template = DOMAIN_TO_TEMPLATE.get(domain, theme)
+    domain = urlparse(link).netloc
 
-    user_file = f"{user_id}_final.json"
+    print(domain)
 
-    try:
-        response = S3_CLIENT.get_object(
-            Bucket=AWS_S3_BUCKET, 
-            Key=user_file
-        )
-
-        resume_data = json.loads(response["Body"].read().decode())
-    except ClientError as e:
-        response = None
-        resume_data = None
-        return ""
+    with open("../resume-builder-frontend/1_final.json") as f:
+        resume_data = json.loads(f.read())
+    
+    template = domain_to_template(domain, resume_data['info']['default_template'])
 
     posting = JobPosting(posting)
 
     build_resume(posting, resume_data)
 
-    builder_class = getattr(import_module("templates." + template), to_camel_case(template))
+    builder_class = getattr(import_module("resume_builder.templates." + template), to_camel_case(template))
 
     builder: LatexTemplate = builder_class("artifacts/resume_result.json", "artifacts/final_doc.tex")
     builder.build_doc()
